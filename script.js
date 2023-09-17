c288d1717ac2c6dba6a1665933fad9810d6b1f3b
sunil kumar patel <92216453+sunil-kumar02@users.noreply.github.com>
2023-09-17 23:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function firstNonRepeatedChar(str) {
+	for(let i = 0; i<str.length; i++){
+		for(int j=i+1 ; j<str.length;i++){
+			if(str.charAT(i) =! str.charAT(j)){
+				console.log(str.charAt(i));
+			}
+		}
+	}
  // Write your code here
 }
 const input = prompt("Enter a string");
